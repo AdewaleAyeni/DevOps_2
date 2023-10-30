c55fad0349e199618c5b38ac321bedf0d20bbc4a
AdewaleAyeni <100167748+AdewaleAyeni@users.noreply.github.com>
2023-10-30 03:19:10 -0500

    Add files via upload

---
+++ V2-EC2.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region     = "us-east-1"   
+  
+}
+
+resource "aws_instance" "demo_server"{
+    ami = "ami-01eccbf80522b562b"
+    instance_type = "t2.micro"             
+    key_name      = "ed-dpt-key" 
+    security_groups =["demo-sg"]        
+
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh port"
+  }
+}
